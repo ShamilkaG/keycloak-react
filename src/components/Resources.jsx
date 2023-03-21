62d2efc96820507183869d07b6b84c90ae11ef1b
shamilka <sham.gurusinghe@gmail.com>
2023-03-21 15:47:20 +0530

    first commit

---
+++ src/components/Resources.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import Keycloak from 'keycloak-js';
+
+export default function Resources(){
+    const [keycloak, setKeycloak] = useState(null)
+    const [authenticated, setAuthenticated] = useState(false)
+
+    useEffect(()=>{
+        const keycloak = new Keycloak ('/keycloak.json');
+        keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+          setKeycloak(keycloak)
+          setAuthenticated(authenticated)
+        })
+      }, [])
+
+      if (keycloak) {
+        if (authenticated) return (
+          {/* JSX returns an image and text as protected **resources** */}
+          <div>
+          <div className='my-12 grid place-items-center'>
+            <p> You are logged in.</p>
+              <div>
+              
+              <img src="https://random.imagecdn.app/500/250"/> 
+              </div>
+          </div>
+          
+        ); 
+        else return (<div className='my-12'>Unable to initiate auth!</div>)
+      }
+    
+      return(
+        <>
+          <div className='my-12'>Keycloak initializing in a moment...</div>
+        </>
+      )
+  }
